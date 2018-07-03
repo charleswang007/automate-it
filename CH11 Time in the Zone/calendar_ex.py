50aa1a44a6dd2555c62a7fa30dcd68cb4aae3ec8
charleswang007 <charleswang007@gmail.com>
2018-07-02 17:09:57 -0700

    update calendar year and month

--- CH11 Time in the Zone/calendar_ex.py
+++ CH11 Time in the Zone/calendar_ex.py
@@ -4,8 +4,8 @@ from calendar import TextCalendar
 cal = TextCalendar()
 
 #Returns the calender for the year 2017
-cal.pryear(2017)
+cal.pryear(2018)
 
 #Returns the calender for the month of Novemeber in 2017
-cal.prmonth(2017, 11)
+cal.prmonth(2018, 6)
 
